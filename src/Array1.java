d2fbfc0a38facbad9731ce5300571f52838a180b
kajela <titoyut56@gmail.com>
2022-05-10 06:06:34 -0400

    Initial commit

---
+++ src/Array1.java
@@ -0,0 +1,12 @@
+public class Array1 {
+    public static void main(String[] args) {
+        int[] anArray;
+        anArray = new int[4];
+        anArray[1] = 10;
+        anArray[2] = 20;
+        anArray[3] = 30;
+        System.out.println("at index of: " +anArray[1]);
+        System.out.println("at index of: " +anArray[2]);
+        System.out.println("at index of: " +anArray[3]);
+    }
+}
